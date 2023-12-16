fbeda4bbf84bf73647da6bdb5a27c00d22f56dc7
Echel Cags <joannacaguco@gmail.com>
2023-12-16 22:52:02 +0800

    added a little bg color and body

--- lib/home.dart
+++ lib/home.dart
@@ -5,6 +5,9 @@ class homePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      backgroundColor: Colors.blueGrey.shade200,
+      body: Column(children: []),
+    );
   }
 }
