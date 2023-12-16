badc83798dd0f4276141e17a02c5bf8f97c94664
Echel Cags <joannacaguco@gmail.com>
2023-12-16 22:26:06 +0800

    created 5 screens

---
+++ lib/home.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class homePage extends StatelessWidget {
+  const homePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
